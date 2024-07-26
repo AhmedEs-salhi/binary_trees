f2d7016efd4a839eea00caeb0b45583825af8065
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-07-26 17:27:44 +0100

    my commit

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that delete a binary tree
+ *
+ * @root: the root of the binary tree
+ *
+ */
+
+void binary_tree_delete(binary_tree_t *root)
+{
+	if (root == NULL)
+		return;
+
+	binary_tree_delete(root->right);
+	binary_tree_delete(root->left);
+
+	free(root);
+	root = NULL;
+}
