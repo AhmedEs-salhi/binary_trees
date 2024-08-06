6f3ddfbe22184a6c36a5608f1ced6459cc950145
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-08-06 18:10:12 +0100

    my commit

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - do the rotate left of a binary tree
+ *
+ * @root: the root node of the binary tree
+ *
+ * Return: the root of the rotated binary tree
+ *
+ */
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *root)
+{
+	binary_tree_t *temp = root;
+
+	if (!root || !temp)
+		return (NULL);
+
+	root = temp->left;
+	temp->parent = root;
+	root->parent = NULL;
+
+	if (root->right != NULL)
+	{
+		temp->left = root->right;
+		root->right->parent = temp;
+	}
+	else
+		temp->left = NULL;
+	root->right = temp;
+
+	return (root);
+}
